31d8c2e470ac1b797c5ebfbf2d6d49ddfd8d932c
Jack Cherng <jfcherng@gmail.com>
2022-03-08 18:54:07 +0800

    refactor: simplify boot.py
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,8 +1,22 @@
-def set_up() -> None:
-    """plugin_loaded"""
-    ...
+# import all listeners and commands
+from .TodoReview import TodoReviewCommand
+from .TodoReview import TodoReviewRenderCommand
+from .TodoReview import TodoReviewResultsCommand
+
+__all__ = (
+    # ST: core
+    "plugin_loaded",
+    "plugin_unloaded",
+    # ST: commands
+    "TodoReviewCommand",
+    "TodoReviewRenderCommand",
+    "TodoReviewResultsCommand",
+)
 
 
-def tear_down() -> None:
-    """plugin_unloaded"""
-    ...
+def plugin_loaded() -> None:
+    pass
+
+
+def plugin_unloaded() -> None:
+    pass
